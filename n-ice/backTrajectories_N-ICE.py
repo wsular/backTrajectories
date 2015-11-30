eb0cd1dcf532e5a605294d5761a71f0859f6eb7f
Von P. Walden <v.walden@wsu.edu>
2015-11-30 10:07:05 -0800

    Moved some files around...

--- n-ice/backTrajectories_N-ICE.py
+++ n-ice/backTrajectories_N-ICE.py
@@ -1,3 +1,4 @@
+import numpy as np
 from netCDF4 import Dataset
 from datetime import datetime
 import pytz
@@ -7,9 +8,9 @@ lance = Dataset('10minute.nc')
 lat   = lance.variables['latitude'][...]
 lon   = lance.variables['longitude'][...]
 utime = lance.variables['unix_time'][...]
-time  = array([datetime.fromtimestamp(t,tz=pytz.UTC) for t in utime])
+time  = np.array([datetime.fromtimestamp(t,tz=pytz.UTC) for t in utime])
 
-stime = array([datetime(2000+int(f.strip()[-10:-8]),int(f.strip()[-8:-6]),int(f.strip()[-6:-4]),int(f.strip()[-2:]),0,0,0,pytz.UTC) for f in open('N-ICEsondeList.dat')])
+stime = np.array([datetime(2000+int(f.strip()[-10:-8]),int(f.strip()[-8:-6]),int(f.strip()[-6:-4]),int(f.strip()[-2:]),0,0,0,pytz.UTC) for f in open('N-ICEsondeList.dat')])
 
 for t in stime[:10]:
 	ind = find(t<=time)[0]
@@ -18,4 +19,3 @@ for t in stime[:10]:
 	hy = hysplit.HYSPLIT4(t,24*7,lat[ind],lon[ind],[0,22,50,100],'N-ICE_nearSfc_')
 	hy.runBackTrajectory()
 	hy.plotBackTrajectory()
-
