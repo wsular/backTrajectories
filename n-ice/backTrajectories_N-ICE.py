87633b1e70e9c6f25b14bb655ee43c9502eb1585
Von P. Walden <v.walden@wsu.edu>
2015-11-30 11:17:14 -0800

    Use np.where instead of find.

--- n-ice/backTrajectories_N-ICE.py
+++ n-ice/backTrajectories_N-ICE.py
@@ -13,7 +13,7 @@ time  = np.array([datetime.fromtimestamp(t,tz=pytz.UTC) for t in utime])
 stime = np.array([datetime(2000+int(f.strip()[-10:-8]),int(f.strip()[-8:-6]),int(f.strip()[-6:-4]),int(f.strip()[-2:]),0,0,0,pytz.UTC) for f in open('N-ICEsondeList.dat')])
 
 for t in stime[:10]:
-	ind = find(t<=time)[0]
+	ind = np.min(np.where(t<=time))
 	print(t,lat[ind],lon[ind])
 
 	hy = hysplit.HYSPLIT4(t,24*7,lat[ind],lon[ind],[0,22,50,100],'N-ICE_nearSfc_')
