73a414f11fc0901fe957b80a848ad4593d25715d
Von P. Walden <v.walden@wsu.edu>
2015-11-30 10:03:03 -0800

    Updated directories to be local files (since difficult to mount N-ICE WP2 drive from aeolus).

--- n-ice/backTrajectories_N-ICE.py
+++ n-ice/backTrajectories_N-ICE.py
@@ -3,13 +3,13 @@ from datetime import datetime
 import pytz
 import hysplit
 
-lance = Dataset('/Volumes/N-ICE/_DATA_FOR_SHARE/Lance N-ICE meteorological and navigation data/10minute.nc')
+lance = Dataset('10minute.nc')
 lat   = lance.variables['latitude'][...]
 lon   = lance.variables['longitude'][...]
 utime = lance.variables['unix_time'][...]
 time  = array([datetime.fromtimestamp(t,tz=pytz.UTC) for t in utime])
 
-stime = array([datetime(2000+int(f.strip()[-10:-8]),int(f.strip()[-8:-6]),int(f.strip()[-6:-4]),int(f.strip()[-2:]),0,0,0,pytz.UTC) for f in open('/Volumes/N-ICE/WP2/Radiosonde/N-ICEsondeList.dat')])
+stime = array([datetime(2000+int(f.strip()[-10:-8]),int(f.strip()[-8:-6]),int(f.strip()[-6:-4]),int(f.strip()[-2:]),0,0,0,pytz.UTC) for f in open('N-ICEsondeList.dat')])
 
 for t in stime[:10]:
 	ind = find(t<=time)[0]
